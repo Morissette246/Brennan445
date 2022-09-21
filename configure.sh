e1e12683cbf98afd3fff81d84b536f67fd939330
Morissette246 <114057695+Morissette246@users.noreply.github.com>
2022-09-21 08:34:18 +0000

    Add files via upload

---
+++ configure.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# V2Ray new configuration
+# Run V2ray
+# Write V2Ray configuration
+cat << EOF > /etc/v2ray/config.json
+{
+    "inbounds": [{
+        "port": 8081,
+        "protocol": "vmess",
+        "settings": {
+            "clients": [{
+                "id": "855af480-4798-4b7b-b143-4194c788c4f3",
+                "alterId": 0
+            }]
+        },
+        "streamSettings": {
+            "network": "ws",
+            "wsSettings": {
+                "path": "/laotou"
+            }
+        }
+    }],
+    "outbounds": [{
+        "protocol": "freedom"
+    }]
+}
+EOF
+
+#Run V2ray
+
+/usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
